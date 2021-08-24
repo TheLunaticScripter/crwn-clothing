f8704225c565aaffffd190ce6fd4a1b22fa65749
John Snow <thelunaticscripter@gmail.com>
2021-08-24 16:29:05 -0400

    Add partial router
    Signed-off-by: John Snow <thelunaticscripter@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+
 import "./App.css";
 import Homepage from "./page/homepage/homepage.component";
+
+const HatsPage = (props) => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <BrowserRouter>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </BrowserRouter>
     </div>
   );
 }
