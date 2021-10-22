a363d39889acdf56eedc5056e655ea0030238fde
John Snow <thelunaticscripter@gmail.com>
2021-10-22 11:27:00 -0400

    updating with shop page and collection preview
    Signed-off-by: John Snow <thelunaticscripter@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Route, BrowserRouter } from "react-router-dom";
 
 import "./App.css";
 import Homepage from "./page/homepage/homepage.component";
-
-const HatsPage = (props) => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from "./page/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </BrowserRouter>
     </div>
   );
