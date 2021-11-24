dd46b874aacba056e67cb92bb6cb387ca4595897
John Snow <thelunaticscripter@gmail.com>
2021-11-24 13:48:29 -0500

    Add initial sign in form
    Signed-off-by: John Snow <thelunaticscripter@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./page/homepage/homepage.component";
 import ShopPage from "./page/shop/shop.component";
+import SignInAndSignUpPage from "./page/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </BrowserRouter>
     </div>
   );
